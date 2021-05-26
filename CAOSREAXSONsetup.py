8a39e293e128a5877ddb812472b42e09a3b96b43
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2021-05-26 15:24:30 -0500

    Update CAOSREAXSONsetup.py

--- CAOSREAXSONsetup.py
+++ CAOSREAXSONsetup.py
@@ -22,3 +22,12 @@ setup(
         'Topic :: Communications'
     ]
 )
+from distutils.core import setup
+import my
+
+setup(name='nightscout-fda-presubmission-01'
+  , version=my.VERSION
+  , description="Nightscout FDA meeting 1"
+  , author="Ben West"
+  , author_email="bewest@gmail.com"
+  )
